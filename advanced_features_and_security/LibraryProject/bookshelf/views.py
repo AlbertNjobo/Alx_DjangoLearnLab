d51e00c263869573f0c38c9869207a09d7b08ecd
Albert Njobo <n02212525l@students.nust.ac.zw>
2025-03-09 11:34:52 -0700

    Task 1 update

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseForbidden
 from .models import Article
 from .models import Book
 from .forms import ArticleForm
+from .forms import ExampleForm
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
