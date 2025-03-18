b6725fdf228988d80a4ee51bd8b21cd5c39107e4
Lawrence Njobo <n02212525l@students.ac.zw>
2025-03-18 05:26:27 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions, filters
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated  # Added missing imports
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework as django_filters  # Added missing import
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
